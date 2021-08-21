b4b88b21d096b48606bbe276f2c83384cd510aed
dilshanihettiarachchi <dilshani.madhuwanthi151@gmail.com>
2021-08-21 22:41:39 +0530

    Implemented APIs for get, get with parameters, update and delete products

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
 const OrderRouter = require("./routes/orders");
+const admin = require("./routes/admin");
 
 // CREATE EXPRESS SERVER
 const app = express();
@@ -13,16 +14,17 @@ app.use(express.json());
 
 // APP ROUTES
 app.use("/orders", OrderRouter);
+app.use("/fury/admin", admin);
 
 mongoose
-   .connect(process.env.DB_CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-   })
-   .then(() => console.log("connected to mongodb"))
-   .catch((err) => console.log("Getting error with: ", err));
+  .connect(process.env.DB_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log("Getting error with: ", err));
 
 // DEFINE PORT
 app.listen(4000, () => {
-   console.log("sever running on port 4000");
+  console.log("sever running on port 4000");
 });
